29d1c2a062c171006878353a0c43d83505d48113
Harsh Kumar Gupta <tron.x.530@gmail.com>
2026-07-06 21:24:27 +0530

    1408-FIND-THE-SMALLEST-DIVISOR-GIVEN-A-THRESHOLD Stats: Time: 11 ms (39.46%), Space: 26.1 MB (47.82%) - LeetHub

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -0,0 +1,28 @@
+int SumByD(vector<int> & nums, int div)
+{
+    int  sum=0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        sum+=ceil(double(nums[i])/double(div));
+    }
+    return sum;
+    
+}
+
+class Solution {
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low=1;
+        int high=*max_element(nums.begin(),nums.end());
+        while (low<=high)
+        {
+            int mid=(low+high)/2;
+            if (SumByD(nums,mid)<=threshold) high=mid-1;
+            
+            else low=mid+1;
+            
+        }
+        return low;
+        
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # LeetCode
 Collection of LeetCode questions to ace the coding interview! - Created using [LeetHub](https://github.com/QasimWani/LeetHub)
+
+<!---LeetCode Topics Start-->
+# LeetCode Topics
+## Array
+|  |
+| ------- |
+| [1408-find-the-smallest-divisor-given-a-threshold](https://github.com/Tron-xR/LeetCode/tree/master/1408-find-the-smallest-divisor-given-a-threshold) |
+## Binary Search
+|  |
+| ------- |
+| [1408-find-the-smallest-divisor-given-a-threshold](https://github.com/Tron-xR/LeetCode/tree/master/1408-find-the-smallest-divisor-given-a-threshold) |
+<!---LeetCode Topics End-->
