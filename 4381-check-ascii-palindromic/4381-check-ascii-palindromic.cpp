de40780b88b2541af6e8d1c4ffe6c5184feea4dd
Harsh Kumar Gupta <tron.x.530@gmail.com>
2026-08-28 23:52:48 +0530

    4381-CHECK-ASCII-PALINDROMIC Stats: Time: 0 ms (100%), Space: 9.2 MB (65.11%) - LeetHub

---
+++ 4381-check-ascii-palindromic/4381-check-ascii-palindromic.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindromic(string s) {
+        string binary = "";
+
+        for (char c : s) {
+            int value = (int)c;
+
+            for (int i = 7; i >= 0; i--) {
+                binary += ((value >> i) & 1) + '0';
+            }
+        }
+
+        int left = 0;
+        int right = binary.size() - 1;
+
+        while (left < right) {
+            if (binary[left] != binary[right]) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
