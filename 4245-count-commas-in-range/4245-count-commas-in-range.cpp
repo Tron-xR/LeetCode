4894b26e9eb07a8d158727e77465ad22eedbfe89
Harsh Kumar Gupta <tron.x.530@gmail.com>
2026-09-08 20:27:38 +0530

    4245-COUNT-COMMAS-IN-RANGE Stats: Time: 0 ms (100%), Space: 8.5 MB (51.38%) - LeetHub

--- 4245-count-commas-in-range/4245-count-commas-in-range.cpp
+++ 4245-count-commas-in-range/4245-count-commas-in-range.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
-    long long countCommas(int n) {
-
-        long long ans = 0;
-
-        long long start = 1000;
-        int commas = 1;
-
-        while (start <= n) {
-
-            long long end = start * 1000 - 1;
-
-            long long countNumbers =
-                min((long long)n, end) - start + 1;
-
-            ans += countNumbers * commas;
-
-            start *= 1000;
-            commas++;
-        }
-
-        return ans;
+    int countCommas(int n) {
+        return max(0, n - 999);
     }
 };
